b2b85a082666da65a280a0bcdafc81a4a8d2e657
Eloi Salles Torres <Eloi Salles Torres>
2017-01-06 00:13:58 +0100

    Add a config.xml file and the FileManager use it.

--- Game/src/SDLTutorial/FileManager.h
+++ Game/src/SDLTutorial/FileManager.h
@@ -1,8 +1,16 @@
 #pragma once
 #include <fstream>
+#include <string>
 #include <iostream>
+#include <sstream>
 #include <map>
+#include <XML\rapidxml.hpp>
+#include <XML\rapidxml_iterators.hpp>
+#include <XML\rapidxml_print.hpp>
+#include <XML\rapidxml_utils.hpp>
+#include "Enumerators.h"
 using namespace std;
+using namespace rapidxml;
 
 struct Score
 {
@@ -26,12 +34,16 @@ class FileManager
 {
 	ofstream *writeBinaryFile;
 	ifstream *readBinaryFile;
+	xml_document<> *doc;
+	xml_node<>*actualNode;
+	xml_node<>*root;
 
 public:
 	FileManager()
 	{
-
+		doc = new xml_document<>();
 	}
+
 	~FileManager()
 	{
 		//Destruim els punter per si de cas.
@@ -77,5 +89,185 @@ public:
 		}
 		file.close();
 		return ran;
+	}
+
+	int PlayerLives(Difficulty diff)
+	{
+		
+		ifstream file("../../res/files/config.xml");
+		stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		string content(buffer.str());
+
+		
+		doc->parse<0>(&content[0]);
+		root = doc->first_node();
+		actualNode = root;
+		actualNode = actualNode->first_node("Difficulty");
+
+		if (diff == Difficulty::EASE)
+		{
+			actualNode = actualNode->first_node("Ease");
+		}
+		else if (diff == Difficulty::MEDIUM)
+		{
+			actualNode = actualNode->first_node("Medium");
+		}
+		else
+		{
+			actualNode = actualNode->first_node("Hard");
+		}
+
+		actualNode = actualNode->first_node("Lives");
+		return atoi(actualNode->value());
+	}
+
+	int CarVelocity(Difficulty diff)
+	{
+		ifstream file("../../res/files/config.xml");
+		stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		string content(buffer.str());
+
+
+		doc->parse<0>(&content[0]);
+		root = doc->first_node();
+		actualNode = root;
+		actualNode = actualNode->first_node("Difficulty");
+
+		if (diff == Difficulty::EASE)
+		{
+			actualNode = actualNode->first_node("Ease");
+		}
+		else if (diff == Difficulty::MEDIUM)
+		{
+			actualNode = actualNode->first_node("Medium");
+		}
+		else
+		{
+			actualNode = actualNode->first_node("Hard");
+		}
+
+		actualNode = actualNode->first_node("CarVelocity");
+		return atoi(actualNode->value());
+	}
+
+	int WoodVelocity(Difficulty diff)
+	{
+		ifstream file("../../res/files/config.xml");
+		stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		string content(buffer.str());
+
+
+		doc->parse<0>(&content[0]);
+		root = doc->first_node();
+		actualNode = root;
+		actualNode = actualNode->first_node("Difficulty");
+
+		if (diff == Difficulty::EASE)
+		{
+			actualNode = actualNode->first_node("Ease");
+		}
+		else if (diff == Difficulty::MEDIUM)
+		{
+			actualNode = actualNode->first_node("Medium");
+		}
+		else
+		{
+			actualNode = actualNode->first_node("Hard");
+		}
+
+		actualNode = actualNode->first_node("WoodVelocity");
+		return atoi(actualNode->value());
+	}
+
+	int TimePlayer(Difficulty diff)
+	{
+		ifstream file("../../res/files/config.xml");
+		stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		string content(buffer.str());
+
+
+		doc->parse<0>(&content[0]);
+		root = doc->first_node();
+		actualNode = root;
+		actualNode = actualNode->first_node("Difficulty");
+
+		if (diff == Difficulty::EASE)
+		{
+			actualNode = actualNode->first_node("Ease");
+		}
+		else if (diff == Difficulty::MEDIUM)
+		{
+			actualNode = actualNode->first_node("Medium");
+		}
+		else
+		{
+			actualNode = actualNode->first_node("Hard");
+		}
+		xml_node<> *divisor = actualNode;
+		actualNode = actualNode->first_node("FroggTime");
+		int num = atoi(actualNode->value());
+
+		divisor = divisor->first_node("DivisorTime");
+		float div = atof(divisor->value());
+
+		return num/div;
+	}
+
+	int RiverElementsForLine(int lvl)
+	{
+		ifstream file("../../res/files/config.xml");
+		stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		string content(buffer.str());
+
+
+		doc->parse<0>(&content[0]);
+		root = doc->first_node();
+		actualNode = root;
+		for (xml_node<> *pNode = root->first_node("Level"); pNode; pNode = pNode->next_sibling())
+		{
+			if (lvl == atoi(pNode->first_attribute()->value()))
+			{
+				actualNode = pNode;
+			}
+		}
+
+		actualNode = actualNode->first_node("RiverElements");
+
+		return atoi(actualNode->value());
+	}
+
+	int RoadElementsForLine(int lvl)
+	{
+		ifstream file("../../res/files/config.xml");
+		stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		string content(buffer.str());
+
+
+		doc->parse<0>(&content[0]);
+		root = doc->first_node();
+		actualNode = root;
+		for (xml_node<> *pNode = root->first_node("Level"); pNode; pNode = pNode->next_sibling())
+		{
+			if (lvl == atoi(pNode->first_attribute()->value()))
+			{
+				actualNode = pNode;
+			}
+		}
+
+		actualNode = actualNode->first_node("RoadElements");
+
+		return atoi(actualNode->value());
 	}
 };
